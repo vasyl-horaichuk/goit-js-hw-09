40a9a5daaed0bbbde72236563013a3ae4664295a
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-12 20:02:14 +0200

    Виконав 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+startBtnRef.disabled = false;
+stopBtnRef.disabled = true;
+
+const onByTargetBgcStart = (e) => {
+    timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyRef.style.backgroundColor = color;
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+}, 1000);
+}
+
+const onByTargetBgcStop = (e) => {
+    startBtnRef.disabled = false;
+    stopBtnRef.disabled = true;
+    clearInterval(timerId);
+    bodyRef.style.backgroundColor = null;
+
+}
+
+startBtnRef.addEventListener('click', onByTargetBgcStart);
+stopBtnRef.addEventListener('click', onByTargetBgcStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
