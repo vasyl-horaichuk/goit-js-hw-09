3c500786c2f3ad9973be38948d4ef71fd325901e
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-16 22:20:47 +0200

    Все виконав

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+  e.preventDefault();
+
+  let delay = e.currentTarget.delay.value;
+  const step = e.currentTarget.step.value;
+  const amount = e.currentTarget.amount.value;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+         }, delay)
+    
+  })
+      
+      .catch(({ position, delay }) => {
+        setTimeout(() => { 
+          Notify.success(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay)
+      });
+    delay += step;
   }
 }
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  let promiseValue = {position, delay};
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(promiseValue);
+    }
+      reject(promiseValue);
+  })
+}
+
+
